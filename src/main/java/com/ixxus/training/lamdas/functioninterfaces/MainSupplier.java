a81a9ccaaa21d74655224316e3732b7a3f193aec
Simon Hutchinson <si@springyweb.com>
2016-04-05 13:53:35 +0100

    Add support for suppliers

---
+++ src/main/java/com/ixxus/training/lamdas/functioninterfaces/MainSupplier.java
@@ -0,0 +1,28 @@
+package com.ixxus.training.lamdas.functioninterfaces;
+
+import java.util.Optional;
+import java.util.UUID;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class MainSupplier {
+	
+	public static void main(String[] args) {
+		
+		//Basic creation
+		Supplier<UUID> randomUuidSupplier = () -> UUID.randomUUID();
+		System.out.println(randomUuidSupplier.get());
+		
+		//commonly used with to create lazy infinite Streams
+		System.out.println(
+				Stream.generate(() -> UUID.randomUUID())
+					  .limit(5)
+					  .collect(Collectors.toList()));
+		
+		//also commonly used to supply an alternative result for an optional
+		System.out.println(
+				Optional.empty()
+						.orElseGet(() -> "Supplied in place of empty option"));
+	}
+}
