35f5adaff3d842df804aaae5ea758955366987f2
Simon Hutchinson <si@springyweb.com>
2016-04-05 11:36:01 +0100

    Add an example of IntConsumer

--- src/main/java/com/ixxus/training/lamdas/functioninterfaces/MainConsumer.java
+++ src/main/java/com/ixxus/training/lamdas/functioninterfaces/MainConsumer.java
@@ -3,6 +3,7 @@ package com.ixxus.training.lamdas.functioninterfaces;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.IntConsumer;
 
 public class MainConsumer {
 	
@@ -18,5 +19,16 @@ public class MainConsumer {
 		//Consumer<T> has a default method andThen which allows consumers to be composed
 		Consumer<String> furtherGreeting = sayHello.andThen(s -> System.out.println("How's things in the world of " + s +"?"));
 		furtherGreeting.accept("Fred");
-	}	
+		
+		//Autoboxing will work with primitives
+		Consumer<Integer> printInt = i -> System.out.println("Hello " + i);
+		printInt.accept(2); 
+		
+		//Use specialised primitive implementation to prevent lots of autoboxing
+		IntConsumer primitivePrintInt = i -> System.out.println("Hello " + i);
+		int[] massiveIntArray = {1,2,3};
+		for (int j : massiveIntArray) {
+			primitivePrintInt.accept(j);
+		}
+	}
 }
