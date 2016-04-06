ddba392b9a8b5b03f81d7cb4739bf0a5a62bf6f6
Simon Hutchinson <si@springyweb.com>
2016-04-06 14:47:42 +0100

    Add contrived example where compiler cannot infer

---
+++ src/main/java/com/ixxus/training/lamdas/syntax/CannotInfer.java
@@ -0,0 +1,24 @@
+package com.ixxus.training.lamdas.syntax;
+
+
+public class CannotInfer {
+	
+	static class Value<T> {
+	}
+
+	@FunctionalInterface
+	interface A<T> {
+	  T apply(Value<T> value);
+	}
+
+	static class B {
+	  public static <T> T test(A<T> callback) {
+		  return null;
+	  }
+	}
+	
+	//Remove the (A<Boolean>) and it cannot infer the boolean 
+	void test() {
+	  B.test((A<Boolean>) value -> true).booleanValue();
+	}
+}
