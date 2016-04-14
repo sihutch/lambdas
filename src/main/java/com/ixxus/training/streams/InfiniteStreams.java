66d243a634ffb9e296359a2ed5c8f26f966ee7a7
Simon Hutchinson <si@springyweb.com>
2016-04-14 16:48:44 +0100

    Add model answers to Stream question

--- src/main/java/com/ixxus/training/streams/InfiniteStreams.java
+++ src/main/java/com/ixxus/training/streams/InfiniteStreams.java
@@ -2,43 +2,39 @@ package com.ixxus.training.streams;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class InfiniteStreams {
-	
+
 	public static void main(String[] args) {
 		getPrimes(0, 10);
 	}
-	
+
 	/**
 	 * @param fromNum
 	 * @param count
 	 * @return count prime numbers starting at fromNum
 	 */
-	public static List<Integer> getPrimes (int fromNum, int count ) {
-		return Stream.iterate(getNextPrime(fromNum -1), i -> getNextPrime(i))
-				     .peek(System.out::println)
-				     .limit(count) //Comment out the limit to show this is infinite
-				     .collect(Collectors.toList());
+	public static List<Integer> getPrimes(int fromNum, int count) {
+		return Stream.iterate(getNextPrime(fromNum - 1), i -> getNextPrime(i))
+				.peek(System.out::println)
+				.limit(count) // Comment out the limit to show this is infinite
+				.collect(Collectors.toList());
 	}
-	
+
 	public static int getNextPrime(int i) {
-		if (isPrimeNumber(i + 1)) {
-			return i + 1;
+		int nextNum = i + 1;
+		if (isPrimeNumber(nextNum)) {
+			return nextNum;
 		} else {
-			return getNextPrime(i + 1);
+			return getNextPrime(nextNum);
 		}
 	}
 
 	public static boolean isPrimeNumber(int i) {
-		if (i < 2) {
-			return false;
-		}
-		for (int d = 2; d <= (int) Math.sqrt(i); d++) {
-			if (i % d == 0) {
-				return false;
-			}
-		}
-		return true;
+		return i > 1 && IntStream
+						.rangeClosed(2, (int) Math.sqrt(i))
+						.noneMatch(d -> i % d == 0);
 	}
 }
--- src/test/java/com/ixxus/training/streams/test/InfiniteStreamsTest.java
+++ src/test/java/com/ixxus/training/streams/test/InfiniteStreamsTest.java
@@ -3,14 +3,17 @@ package com.ixxus.training.streams.test;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
 
 import com.ixxus.training.streams.InfiniteStreams;
 
 public class InfiniteStreamsTest {
 	
-	private static int[] EXPECTED_PRIMES = {2,3,5,7,11,13,17,19,23,29};
-	private static int[] NON_PRIMES = {1,4,6,8,9,10,12,14,15,16,18,20,21,22};
+	private static List<Integer> EXPECTED_PRIMES = Arrays.asList(2,3,5,7,11,13,17,19,23,29);
+	private static List<Integer> NON_PRIMES = Arrays.asList(1,4,6,8,9,10,12,14,15,16,18,20,21,22);
 	
 	@Test
 	public void isPrimeNumber() {
@@ -25,4 +28,18 @@ public class InfiniteStreamsTest {
 			assertThat(i + " is not prime", InfiniteStreams.isPrimeNumber(i), equalTo(false));
 		}
  	}
+	
+	@Test
+	public void nextPrimeFromAGivenNumber() {
+		for(int i =0; i<EXPECTED_PRIMES.size()-1 ;i++) {
+			int num = EXPECTED_PRIMES.get(i);
+			int expectedNextPrime = EXPECTED_PRIMES.get(i+1);
+			assertThat("Next prime is incorrect", InfiniteStreams.getNextPrime(num), equalTo(expectedNextPrime));
+		}
+ 	}
+	
+	@Test
+	public void theRequestedNumberOfPrimesAreReturnedInOrder() {
+		assertThat("Primes not returned as expected", InfiniteStreams.getPrimes(1, 10), equalTo(EXPECTED_PRIMES));
+	}
 }
