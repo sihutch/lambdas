2eb42e34758b493984375af3bf4909882f8ca611
Simon Hutchinson <si@springyweb.com>
2016-05-04 16:07:45 +0100

    Rename class

--- src/main/java/com/ixxus/training/lambdas/anonymous/LambdaSquareMain.java
+++ src/main/java/com/ixxus/training/lambdas/anonymous/LambdaSquareMain.java
@@ -2,7 +2,7 @@ package com.ixxus.training.lambdas.anonymous;
 
 import java.util.function.Function;
 
-public class LamdaSquareMain {
+public class LambdaSquareMain {
 	
 	public static void main(String[] args) {
 		Function<Integer, Integer> func = (Integer x) -> {return x * x;};
